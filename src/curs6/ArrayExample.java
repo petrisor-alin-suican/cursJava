136d65a7188a5411b4689fa35f3516f4fa175105
petrisor alin suican <petrisor-alin.suican@db.com>
2023-05-26 17:53:49 +0300

    curs6

---
+++ src/curs6/ArrayExample.java
@@ -0,0 +1,38 @@
+package curs6;
+
+public class ArrayExample {
+
+	public static void main(String[] args) {
+
+		
+		  String[] textArray = new String[5];
+	   // String [] textArray = {null, null, null, null, null};
+	  //index :                   0     1     2     3     4  
+		  System.out.println(textArray[1]);
+		  textArray[1] = "Maria";
+		// String [] textArray = {null, Maria, null, null, null};
+		  System.out.println(textArray[1]);
+		  System.out.println(textArray[3]);
+		  textArray[3] = "Oana";
+		// String [] textArray = {null, Maria, null, Oana, null};
+		  System.out.println(textArray[3]);
+		  
+		  
+		  
+		  System.out.println("-------------------------");
+		  
+		 // System.out.println(textArray[5]);
+		  
+		  
+		  
+		  try { 
+			  System.out.println(textArray[5]);
+		  }catch(Exception e) {
+			  System.out.println("Ai incercat sa accesezi in afara array-ului");
+		  }
+		  System.out.println("Alt cod");
+		  
+		  
+	}
+
+}
