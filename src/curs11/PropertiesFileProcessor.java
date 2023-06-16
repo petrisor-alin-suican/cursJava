ee63eb2502450b940140bbfb83bc4d1c2c6f8f43
petrisor alin suican <petrisor-alin.suican@db.com>
2023-06-16 16:47:49 +0300

    curs 11 intermediar

---
+++ src/curs11/PropertiesFileProcessor.java
@@ -0,0 +1,94 @@
+package curs11;
+
+import java.io.*;
+import java.util.Properties;
+
+public class PropertiesFileProcessor {
+	
+	
+	
+
+	public void writePropertiesFile() throws IOException {
+		
+		try (OutputStream outPutStream = new FileOutputStream("test.properties")) {
+			
+			Properties propFile = new Properties();
+			propFile.setProperty("user", "testuser");
+			propFile.setProperty("pass", "pass1234");
+			propFile.setProperty("email", "testuser@test.com");
+			propFile.store(outPutStream, "am salvat fisier properties");
+			//outPutStream.close();
+			
+		} catch (FileNotFoundException e) {
+		
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	public void readPropertiesFile(String key) {
+		
+		try (FileInputStream inputStream = new FileInputStream("test.properties")){
+			Properties prop = new Properties();
+			prop.load(inputStream);
+			
+			String value = prop.getProperty(key);
+			System.out.println(value);
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	public void updatePropertiesFile(String key, String value) {
+		
+		Properties prop = new Properties();
+		
+		
+		
+		try (FileInputStream inputStream = new FileInputStream("test.properties")) {
+			
+			prop.load(inputStream);
+			
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		
+		try(OutputStream output = new FileOutputStream("test.properties")){
+			
+			prop.setProperty(key, value);
+			prop.store(output, null);
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void deleteFromPropertiesFile(String key) {
+        Properties prop = new Properties();
+		
+		
+		//citesc fisierul
+		try (FileInputStream inputStream = new FileInputStream("test.properties")) {
+			
+			prop.load(inputStream);   //incarca fisierul in obiectul de tip properties
+			
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		//salvez noile modificari
+        try(OutputStream output = new FileOutputStream("test.properties")){
+			
+			prop.remove(key);           //sterge cheia si implicit valoarea asociata
+			prop.store(output, null);   //salveaza modificarile pe output string
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
